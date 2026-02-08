a9039cc97b8a73d91fa72366ffdca82d36a4360c
mohdshahid192-guthub <shahidroast192@gmail.com>
2026-02-08 19:09:53 +0530

    login_form_background_change

--- landing_page/landing.js
+++ landing_page/landing.js
@@ -1,6 +1,10 @@
 const signIn = document.querySelector(".sign-in");
-
+const signUp = document.querySelector(".sign-up");
 
 signIn.addEventListener("click", () =>{
   window.location.href = "/Login_page/login.html";
 } );
+
+signUp.addEventListener("click", () =>{
+  window.location.href = "/new_user/signUp.html";
+})
